3c101e2aad094a844d867a923acca357b046fbd6
olegtr <oleg.trygub@hotmail.com>
2016-08-29 22:39:20 -0400

    added examples

--- solution.py
+++ solution.py
@@ -1,71 +1,111 @@
-
-CONST_MISS = 0
-CONST_HIT = 1
-CONST_SANK = 2
-CONST_TAKEN = 3
-CONST_WIN = 4
-
 import sys
 
+class Result:
+    Miss, Hit, Sank, Taken, Win = ["miss", "hit", "sank", "double shot", "win"]
+
 class Field:
-    def __init__(self, data = , ships = None):
-        self.ships = ships
-        self.nships = len(ships)
+    def visit(self, i, j, cnt):
+        if self.map[i][j] >= 0:
+            return
+        self.map[i][j] = cnt
+        self.ships[cnt-1].add((i, j))
+        if i > 0:
+            self.visit(i-1, j, cnt)
+        if j > 0:
+            self.visit(i, j-1, cnt)
+        if i < self.size - 1:
+            self.visit(i+1, j, cnt)
+        if j < self.size - 1:
+            self.visit(i, j+1, cnt)
 
-    def hit(self, x, y):
-        if self.map[x][y] < 0:
-            return CONST_TAKEN
-        if self.map[x][y] == 0:
-            return CONST_MISS
+    def load_map(self):
+        cnt = 0
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.map[i][j] == -1:
+                    cnt += 1
+                    self.ships.append(set([]))
+                    self.visit(i, j, cnt)
 
-        ship = ships[self.map[x][y] - 1]
+    def __init__(self, battlemap):
+        self.map = battlemap
+        self.size = len(battlemap)
+        self.ships = []
+        self.load_map()
+        self.nships = len(self.ships)
+
+    def hit_ship(self, x, y):
+        ship = self.ships[self.map[x][y] - 1]
         self.map[x][y] *= -1
         if len(ship) < 2:
             ship.clear()
             self.nships -= 1
-            if self.nships != 0:
-                return CONST_SANK
-            return CONST_WIN
-        else:
-            ship.remove((x, y))
-            return CONST_HIT
+            if self.nships == 0:
+                return Result.Win
+            return Result.Sank
 
-def is_coordinate(s, m = None):
-    try:
-        val = int(s)
-        return val > 0 and (m == None or val <= m)
-    except ValueError:
-        return False
+        ship.remove((x, y))
+        return Result.Hit
 
+    def hit(self, x, y):
+        if self.map[x][y] < 0:
+            return Result.Taken
+        if self.map[x][y] == 0:
+            return Result.Miss
+
+        return self.hit_ship(x, y)
+
+def Field_tests():
+    field = Field([[-1, 0, 0], [0, -1, 0], [0, 0, -1]])
+    assert len(field.ships) == 3
+    field = Field([[0, -1, 0], [0, -1, 0], [0, 0, -1]])
+    assert len(field.ships) == 2
+    field = Field([[0, 0, -1, 0, -1], [-1, 0, 0, 0, -1], [-1, 0, 0, 0, 0], [-1, 0, -1, 0, -1], [0, 0, -1, 0, 0]])
+    assert len(field.ships) == 5
+    assert field.hit(1, 0) == Result.Hit
+    assert field.hit(1, 1) == Result.Miss
+    assert field.hit(2, 0) == Result.Hit
+    assert field.hit(3, 0) == Result.Sank
+    assert field.nships == 4
+    assert field.hit(4, 2) == Result.Hit
+    assert field.hit(2, 2) == Result.Miss
+    assert field.hit(0, 4) == Result.Hit
+    assert field.hit(3, 4) == Result.Sank
+    assert field.nships == 3
+    assert field.hit(3, 2) == Result.Sank
+    assert field.nships == 2
+    assert field.hit(0, 2) == Result.Sank
+    assert field.nships == 1
+    assert field.hit(1, 4) == Result.Win
+    assert field.nships == 0
+    field = Field([[ 0,  0,  0,-1, -1], \
+                   [ 0,  0,  0, 0, -1], \
+                   [-1,  0,  0, 0,  0], \
+                   [-1,  0, -1, 0,  0], \
+                   [-1, -1, -1, 0,  0]])
+    assert field.nships == 2
+
+def read_map_from_file(path):
+    input_file = open(path, 'r')
+    battlemap = []
+    size = int(input_file.readline())
+    for i in range(size):
+        battlemap.append(map(lambda c: 0 if int(c) == 0 else -1, input_file.readline().strip()))
+    input_file.close()
+    return battlemap
 
 def run(fields):
     turn = 0
     while True:
-        status = "Player " + str(turn + 1) + " "
-        print status + " turn:"
+        print "Player " + str(turn + 1) + " turn:"
         coordinates = raw_input().split(" ")
-        if len(coordinates) < 2:
-
-        x, y = int(coordinates[0]), int(coordinates[1])
+        y, x = int(coordinates[0]) -  1, int(coordinates[1]) - 1
+        print fields[1 - turn].ships
         result = fields[1 - turn].hit(x, y)
-        if result == CONST_WIN:
-            print status + "wins"
+        print result
+        if result == Result.Win:
             return
-        if result == CONST_SANK:
-            print status + "sank a ship"
-        elif result == CONST_HIT:
-            print status + "hit a ship"
-        elif result == CONST_MISS:
+        if result == Result.Miss or result == Result.Taken:
             turn = 1 - turn
-            print status + "misses"
-        elif result == CONST_TAKEN:
-            turn = 1 - turn
-            print status + "misses, double shot"
 
-
-field = [[0, 0, 2, 0, 4], \
-          [1, 0, 0, 0, 4], \
-          [1, 0, 0, 0, 0], \
-          [1, 0, 3, 0, 5], \
-          [0, 0, 3, 0, 0]]
-ships = [[(1, 1), (1, 2), (1, 3)], [(0, 2)], [(3, 2), (4, 2)], [(0, 4), (1, 4)]]
+run(Field(sys.argv[1]), Field(sys.argv[2]))
